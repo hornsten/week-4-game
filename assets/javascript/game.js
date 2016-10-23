1e46321fb5ae6f51e7629b518138e95e1b63d239
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-10-23 12:07:50 -0500

    Adds images and characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,60 +3,67 @@ $(document).ready(function () {
 
 var character = {
 
-	guyA: {
-		name: 'Guy A',
+	beatrix: {
+		name: 'Beatrix Kiddo',
+		url: 'assets/images/beatrix.jpeg',
 		health: 180,
 		power: 8
 	},
 
-	guyB: {
-		name: 'Guy B',
+	elle: {
+		name: 'Elle Driver',
+		url: 'assets/images/elle.jpeg',
 		health: 100,
 		power: 10
 	},
 
-	guyC: {
-		name: 'Guy C',
+	vernita: {
+		name: 'Vernita Green',
+		url: 'assets/images/vernita.jpeg',
 		health: 200,
 		power: 5
 	},
 
-	guyD: {
-		name: 'Guy D',
+	oren: {
+		name: 'O-ren Ishii',
+		url: 'assets/images/oren.jpeg',
 		health: 160,
 		power: 15
 	}
 }
 
-var hero, isHeroChosen, isDefenderChosen, heroLoses, defenderLoses, attackDefender;
+
+var hero, defender, isHeroChosen, isDefenderChosen, heroLoses, defenderLoses, attackDefender;
+initializeGame();
 
 function initializeGame() {
 
 	isHeroChosen = false;
 	isDefenderChosen = false;
-	attackDefender = false;
 	heroLoses = false;
 	defenderLoses = false;
 
 	$('.defender,.stage').empty();
+
+	document.getElementById("beatrix").src = character.beatrix.url;
+	document.getElementById("elle").src = character.elle.url;
+	document.getElementById("vernita").src = character.vernita.url;
+	document.getElementById("oren").src = character.oren.url;
+
+
+
 }
 
 //Click event to choose hero. Others are moved to the staging row//
-$('.character').one('click', function() {
-		
+$('.character').on('click', function() {
 
-		$('.character').not(this).appendTo('.stage');
-		$(this).removeClass('character');
+
+		$('.character').not(this).appendTo('.stage').addClass('nerds');
 		
 
 });
 
-$('.stage').on('click',function() {
 
-	$(event.target).appendTo('.defender');
-})
 
-// call initializeGame to prepare game for play
-    initializeGame();
 });
 
