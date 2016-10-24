e5b03ec81b68ee3f3361c67a48815c6dcdb06e29
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-10-23 21:00:36 -0500

    Working on functions using data from the four divs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,16 @@
 
 $(document).ready(function () {
 
-
-var character = {
-
-
-
-	beatrix: {
-		name: 'Beatrix Kiddo',
-		url: 'assets/images/beatrix.jpeg',
-		health: 180,
-		power: 8
-	},
-
-	elle: {
-		name: 'Elle Driver',
-		url: 'assets/images/elle.jpeg',
-		health: 100,
-		power: 10
-	},
-
-	vernita: {
-		name: 'Vernita Green',
-		url: 'assets/images/vernita.jpeg',
-		health: 200,
-		power: 15
-	},
-
-	oren: {
-		name: 'O-ren Ishii',
-		url: 'assets/images/oren.jpeg',
-		health: 160,
-		power: 15
-	}
-}
-
 $("#b").data({"name":"Beatrix Kiddo","health":180,"power": 8, "url":'assets/images/beatrix.jpeg'});
-$("#e").data({"name":"Elle Driver","health":100,"power": 10});	
-$("#v").data({"name":"Vernita Green","health":200,"power": 15});	
-$("#o").data({"name":"Oren Ishii","health":160,"power": 20});	
+$("#e").data({"name":"Elle Driver","health":100,"power": 10, "url":'assets/images/elle.jpeg'});	
+$("#v").data({"name":"Vernita Green","health":200,"power": 15, "url":'assets/images/vernita.jpeg'});	
+$("#o").data({"name":"Oren Ishii","health":160,"power": 20, "url":'assets/images/oren.jpeg'});	
 
 // $(".character").click(function(){
 // 	alert($(this).data("health"));
 // });
 
-var hero, defender, isHeroChosen, isDefenderChosen, heroLoses, defenderLoses, attackDefender;
+var heroHealth, defenderHealth, heroPower, defenderPower,heroId, defenderId, isHeroChosen, isDefenderChosen, heroLoses, defenderLoses, attackDefender;
 initializeGame();
 
 function initializeGame() {
@@ -55,16 +21,16 @@ function initializeGame() {
 	defenderLoses = false;
 
 
-	document.getElementById("beatrix").src = character.beatrix.url;
+	document.getElementById("beatrix").src = $("#b").data("url");
 	document.getElementById("bname").innerHTML = $("#b").data("name");
 	document.getElementById("bhealth").innerHTML = $("#b").data("health");
-	document.getElementById("vernita").src = character.vernita.url;
+	document.getElementById("vernita").src = $("#v").data("url");
 	document.getElementById("vname").innerHTML = $("#v").data("name");
 	document.getElementById("vhealth").innerHTML = $("#v").data("health");
-	document.getElementById("elle").src = character.elle.url;
+	document.getElementById("elle").src = $("#e").data("url");
 	document.getElementById("ename").innerHTML = $("#e").data("name");
 	document.getElementById("ehealth").innerHTML = $("#e").data("health");
-	document.getElementById("oren").src = character.oren.url;
+	document.getElementById("oren").src = $("#o").data("url");
 	document.getElementById("oname").innerHTML = $("#o").data("name");
 	document.getElementById("ohealth").innerHTML = $("#o").data("health");
 }
@@ -81,44 +47,49 @@ $('.character').on('click', function() {
 		$(this).css('background-color', 'black');
 		$(this).css('color', 'white');
 		isDefenderChosen = true;
-		alert($(this).data("health"));
+		defender = ($(this).data("name"));
+		defenderHealth = ($(this).data("health"));
+		defenderPower = ($(this).data("power"));
+		defenderId = ($(this).attr('id'));
 		
 	} else {
 		$(this).addClass('hero');
 		$('.character').not(this).appendTo('.stage');
 		$('.character').not(this).css('background-color', 'red');
 		isHeroChosen=true;
-		alert($(this).data("health"));
-	}
+		hero = ($(this).data("name"));
+		heroHealth = ($(this).data("health"));
+		heroPower = ($(this).data("power"));
+		heroId = ($(this).attr('id'));
 
-
+}
 });
 
 
 $('#attack').on('click', function() {
 
-	if (character.elle.health <= 0) {
+	if (heroHealth <= 0) {
 		document.getElementById("fight status").innerHTML = "You lose."
 		return;
 
-	} else if (character.oren.health <=0) {
+	} else if (defenderHealth <=0) {
 			isDefenderChosen = false;
 			document.getElementById("fight status").innerHTML = "You win!"
 		}
 
 	 else {
 
-		character.elle.power += character.elle.power;
-	character.elle.health -= character.oren.power;
-	console.log("Elle Power: " + character.elle.power);
-	document.getElementById("ehealth").innerHTML = character.elle.health;
+		heroPower += heroPower;
+	heroHealth -= defenderPower;
+	alert("Hero Power: " + heroPower);
+	document.getElementById(heroId).innerHTML = heroHealth;
 
-	character.oren.health -= character.elle.power;
-	console.log("Oren Power: " + character.oren.power);
-	document.getElementById("ohealth").innerHTML = character.oren.health;
-	document.getElementById("fight status").innerHTML = "You have attacked Oren" +
-	" for " + character.elle.power + " damage. Oren has attacked you for " +
-	character.oren.power + " damage."
+	defenderHealth -= heroPower;
+	alert("Defender Health: " + defenderHealth);
+	document.getElementById(defenderId).innerHTML = heroHealth;
+	document.getElementById("fight status").innerHTML = "You have attacked " + defender + " for " + heroPower + " damage." + defender + " has attacked you for " +
+	defenderPower + " damage."
+
 	};
 	
 })
